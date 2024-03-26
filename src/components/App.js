c40796c53bdde2b95dcf3cd81138f047d6c00af8
Canberk Akartuna <canberkakartuna@hotmail.com>
2024-03-26 17:55:06 +0300

    basic react webpack template

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App!!!</h1>
+      {/* Add your components and content here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 2024,
+    hot: true,
+    open: true,
+  },
+});
